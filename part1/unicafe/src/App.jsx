ac77a8dd1f7e83dff5def4bfdfbe3661c83dbd89
Jeroen <jeroenwsteens@gmail.com>
2024-11-09 20:07:53 +0100

    Exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,13 +2,23 @@ import { useState } from 'react'
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(6)
+  const [neutral, setNeutral] = useState(2)
+  const [bad, setBad] = useState(1)
+  const total = good + neutral + bad
+  const average = (good - bad) / total
+  const positive = good / total
 
   return (
     <div>
-      code here
+      <p>
+        Good: {good}<br/>
+        Neutral: {neutral}<br/>
+        Bad: {bad}<br/>
+        Total: {total}<br/>
+        Average: {average}<br/>
+        Positive: {positive}%
+      </p>
     </div>
   )
 }
