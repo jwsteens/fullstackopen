f3f12f3aacd45f8d8cefb09590e2ddbd545f61c5
Jeroen <jeroenwsteens@gmail.com>
2024-11-09 20:13:29 +0100

    Exercise 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,6 +5,12 @@ const Statistics = ({good, neutral, bad}) => {
   const average = (good - bad) / total
   const positive = good / total
 
+  if (total === 0) {
+    return (
+      <p>No feedback given yet. Will you be the first?</p>
+    )
+  }
+
   return (
     <p>
       Good: {good}<br/>
@@ -19,9 +25,9 @@ const Statistics = ({good, neutral, bad}) => {
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(6)
-  const [neutral, setNeutral] = useState(2)
-  const [bad, setBad] = useState(1)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
