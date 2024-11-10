d2174a300f97425237e19de8f80f186cc4d80364
Jeroen <jeroenwsteens@gmail.com>
2024-11-10 14:51:47 +0100

    Notes exercise (part2c)

---
+++ part2/notes/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import Note  from './components/Note'
+
+
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('A new note...')
+  const [importantNotesOnly, setImportantNotesOnly] = useState(false)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < .5,
+      id: String(notes.length + 1)
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)    
+  }
+
+  const notesToShow = importantNotesOnly ? notes.filter(note => note.important) : notes
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <button onClick={() => setImportantNotesOnly(!importantNotesOnly)}>Show {importantNotesOnly ? "all" : "important"} notes</button>
+      <ul>
+        {notesToShow.map(note => <Note key={note.id} note={note} /> )}
+      </ul>
+
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type='submit'>save</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
