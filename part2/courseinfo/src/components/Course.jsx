63987f266349b5b1fd9e9a8c276f650c3334d7bf
Jeroen <jeroenwsteens@gmail.com>
2024-11-10 12:52:01 +0100

    Exercise 2.3 - 2.5

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,22 +1,12 @@
-const Content = ({ course }) => {
+const Course = ({ course }) => {
     const totalExercises = course.parts.map(part => part.exercises).reduce((partialSum, value) => partialSum + value)
-    
     return (
         <div>
+            <h1>{course.name}</h1>
             <ul>
                 {course.parts.map(part => <li key={part.id}>{part.name} {part.exercises}</li>)}
             </ul>
             <p>Total of {totalExercises} exercises</p>
-        </div>
-    )
-}
-
-const Course = ({ course }) => {
-    
-    return (
-        <div>
-            <h1>Half Stack application development</h1>
-            <Content course={course} />
         </div>
     )
 }
