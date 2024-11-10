2a8060ed98852521fd51b146f13f719069c4dcc3
Jeroen <jeroenwsteens@gmail.com>
2024-11-10 16:26:55 +0100

    Notes

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Note  from './components/Note'
 import axios from 'axios'
+import noteService from './services/notes'
 
 
 
@@ -10,10 +11,12 @@ const App = () => {
   const [importantNotesOnly, setImportantNotesOnly] = useState(false)
 
   useEffect(() => {
-    console.log("effect");
-    axios
-      .get('http://localhost:3001/notes')
-      .then(res => setNotes(res.data))
+    noteService
+      .getAll()
+      .then(res => setNotes(res))
+      .catch(error => {
+        setNotes([{ id: "1", content: error.message }])
+      })
   }, [])
 
   const addNote = (event) => {
@@ -25,12 +28,30 @@ const App = () => {
       id: String(notes.length + 1)
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    noteService
+      .create(noteObject)
+      .then(res => {
+        setNotes(notes.concat(res))
+        setNewNote('')
+      })
+      .catch(() => {})
   }
 
   const handleNoteChange = (event) => {
     setNewNote(event.target.value)    
+  }
+
+  const toggleNoteImportance = (id) => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find( n => n.id === id )
+
+    noteService
+      .update(id, { ...note, important: !note.important })
+      .then(res => {
+        const newNotes = notes.map(n => n.id === id ? res : n)
+        setNotes(newNotes)
+      })
+      .catch(() => {})
   }
 
   const notesToShow = importantNotesOnly ? notes.filter(note => note.important) : notes
@@ -40,7 +61,7 @@ const App = () => {
       <h1>Notes</h1>
       <button onClick={() => setImportantNotesOnly(!importantNotesOnly)}>Show {importantNotesOnly ? "all" : "important"} notes</button>
       <ul>
-        {notesToShow.map(note => <Note key={note.id} note={note} /> )}
+        {notesToShow.map(note => <Note key={note.id} note={note} toggleImportance={() => toggleNoteImportance(note.id)} /> )}
       </ul>
 
       <form onSubmit={addNote}>
--- part2/notes/src/components/Note.jsx
+++ part2/notes/src/components/Note.jsx
@@ -1,6 +1,9 @@
-const Note = ({ note }) => {
+const Note = ({ note, toggleImportance }) => {
     return (
-      <li>{note.content}</li>
+      <li>
+        {note.content}
+        <button onClick={toggleImportance}>{note.important ? 'Mark not important' : 'Mark important'}</button>
+      </li>
   )
 }
 
